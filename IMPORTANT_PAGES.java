e3c5478d3a58a3a550083cf86a60baadc2a66e39
Shruti Srivastava <shrutishreya222@gmail.com>
2022-09-22 23:42:56 +0530

    prob3

---
+++ IMPORTANT_PAGES.java
@@ -0,0 +1,19 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc =new Scanner(System.in);
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    if(A==0){
+		        System.out.println("https://www.codechef.com/practice");
+		    }
+		     else if (A==1 && B==0){
+		        System.out.println("https://www.codechef.com/contests");
+		    }
+		    else if(A==1 && B==1){
+		        System.out.println("https://discuss.codechef.com");
+		    }
+	}
+}
